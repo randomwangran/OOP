40d32e8bfe188f37af00ae4099d4a831ed38d0e3
Ran Wang <wang1hf@uwindsor.ca>
2018-09-27 21:16:01 -0400

    revise project folder

---
+++ Unreal/Section03/Source/Section03/Section03GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Section03GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION03_API ASection03GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
